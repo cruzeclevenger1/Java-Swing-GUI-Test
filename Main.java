3e56a15b8381fa2a9df0caac6966c9bd452a2a88
Cruze Clevenger <98177813+cruzeclevenger1@users.noreply.github.com>
2022-01-24 18:49:41 +0000

    Minor Changes

--- Main.java
+++ Main.java
@@ -14,7 +14,6 @@ class Main {
 
     JFrame myframe = new JFrame();
 
-    
     myframe.setSize(300, 300);
     myframe.setTitle("Blank Window");
     myframe.setResizable(false);
@@ -24,6 +23,9 @@ class Main {
     //*******************************************
 
     boolean done = false;
+    boolean winopen = false;
+
+    System.out.println("Type commands here. Type 'help' for list of commands.");
 
 
     while (done == false) {
@@ -36,11 +38,15 @@ class Main {
         System.out.println("Opening Window...");
         System.out.println("Done!");
 
+        winopen = true;
+
         myframe.setVisible(true);
       }
 
       if (coninput.equals("window close")) {
         System.out.println("Closing Window...");
+
+        winopen = false;
 
         myframe.setVisible(false);
         System.out.println("Done!");
@@ -53,8 +59,28 @@ class Main {
         System.out.println("Done!");
       }
 
+      if (coninput.equals("help")) {
+        System.out.println("Commands: ");
+        System.out.println("window open: opens a window");
+        System.out.println("window close: closes the open window");
+        System.out.println("exit: shuts down the program");
+        System.out.println("help: lists commands");
+        //System.out.println("");
+        //System.out.println("");
+        //System.out.println("");
+      }
+
+      **if (coninput.equals("change text") && winopen == true) {
+        System.out.print("What do you want the text to say > > > ");
+        JLabel text1 = new JLabel(input.nextLine());
+        System.out.println("Adding...");
+        myframe.add(text1);
+      }**
+
+
+
+      if (coninput.equals("change text") && winopen == false) {
+        System.out.print("You have to have a window open.");
+      }
+
     }
-
-
-  }
-}
